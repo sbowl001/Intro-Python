2ecde8d7cff7613f5d63907e99a344f23b602fa7
Stephanie Bowles <sbowl001@fiu.edu>
2018-08-02 13:07:24 +0900

    complete tuples

--- src/day-1-toy/tuples.py
+++ src/day-1-toy/tuples.py
@@ -28,11 +28,11 @@ def print_tuple(t):
     for i in t:
         print(i)
   # Prints 1 2 5 7 99, one per line
-print(print_tuple(t))
+    print(print_tuple(t))
 
 # Declare a tuple of 1 element then print it
 
 
 u = (1,)  # What needs to be added to make this work?
-#print(u)
+# print(u)
 print_tuple(u)
