9a39ac0dd35c935ad2464de0205638620186e3e5
Stephanie Bowles <sbowl001@fiu.edu>
2018-08-02 12:11:56 +0900

    func done

--- src/day-1-toy/func.py
+++ src/day-1-toy/func.py
@@ -3,4 +3,10 @@
 # Read a number from the keyboard
 num = input("Enter a number: ")
 
+def is_even(num):
+    if int(num) % 2 == 0:
+        return "Even!"
+    else:
+        return "Odd"
+print(is_even(num)) 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
