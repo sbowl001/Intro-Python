c9040b154186c1442cadd1977a95ac3cd136f3cb
Stephanie Bowles <sbowl001@fiu.edu>
2018-08-03 19:03:29 +0900

    fileio working

--- src/day-1-toy/fileio.py
+++ src/day-1-toy/fileio.py
@@ -6,10 +6,14 @@ file = open('foo.txt', 'r')
 for line in file:
     print(line)
 # Close the file
-
+file.close()
 
 # Use open to open file "bar.txt" for writing
-
+file = open('bar.txt', 'w')
 # Use the write() method to write three lines to the file
+file.write("Hi\n")
+file.write("This is\n")
+file.write("a new line")
 
 # Close the file
+file.close()
