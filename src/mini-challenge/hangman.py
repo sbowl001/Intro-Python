c834c13fbcb75252ce77e63ef7a20933ff2b4122
Stephanie Bowles <sbowl001@fiu.edu>
2018-08-06 20:42:24 +0900

    with solution lecture

--- src/mini-challenge/hangman.py
+++ src/mini-challenge/hangman.py
@@ -12,7 +12,7 @@ bodies = [ " ------\n |    |\n |    O\n |\n |\n |\n |\n |\n---",
 " ------\n |    |\n |    O\n |    |\n |    |\n |   / \n |\n |\n---", 
 " ------\n |    |\n |    O\n |    |\n |    |\n |   / \ \n |\n |\n---", 
 " ------\n |    |\n |    O\n |   \|\n |    |\n |   / \ \n |\n |\n---",
-" ------\n |    |\n |  O\n |   \|/\n |    |\n |   / \ \n |\n |\n---" ]
+" ------\n |    |\n |    O\n |   \|/\n |    |\n |   / \ \n |\n |\n---" ]
 strikes = 0
 words = [None]
 file = open("words.txt", "r")
@@ -20,8 +20,8 @@ for line in file:
     words.append(line)
 file.close()
 target_word = words[random.randint(0, len(words))]
-letters_left = len(targetWord)-1
-length = len(targetWord)-1
+letters_left = len(target_word)-1
+length = len(target_word)-1
 cur_word = "_" * length
 alphabet = [chr(65+x) for x in range(0, 26) ]
 
@@ -31,10 +31,10 @@ def drawBody():
 
 # Replace blanks with correctly guessed letters
 def fillLetters( letter ):
-    for i in range(len(targetWord)-1):
-        if( targetWord[i : i+1]) == letter:
+    for i in range(len(target_word)-1):
+        if( target_word[i : i+1]) == letter:
             global cur_word
-            cur_word = cur_word[0: i] + letter + cur_word[i: ]
+            cur_word = cur_word[0: i] + letter + cur_word[i+1: ]
             global letters_left
             letters_left -= 1
 
@@ -46,29 +46,36 @@ def printWord( word ):
     print(prnt_word)
 
 # Begin game
-print( "Welcome to Hangmn!" )
+print( "Welcome to Hangman!" )
 printWord(cur_word)
 drawBody()
 print("Letters left:")
 printWord(alphabet)
-
+printWord(target_word)
 # Gameplay loop
 while strikes < 5 and letters_left > 0:
     letter = input("\nPlease guess a letter...")
-    if letter in targetWord:
+    if not letter.isalpha():
+        print("Please only enter letters")
+    elif len(letter) > 1: 
+        print("Please guess only a single letter.")
+    elif not letter.upper() in alphabet:
+        print("Already guessed this letter!")
+    elif letter in target_word:
         print("Great!")
         fillLetters(letter)
+        alphabet.remove(letter.upper())
     else:
         strikes += 1
         print( str(strikes) + " / 5 strikes" )
-    printWord(curWord)
+        alphabet.remove(letter.upper())
+    printWord(cur_word)
     drawBody()
-    alphabet.remove(letter.upper())
     print("Letters left:")
     printWord(alphabet)
 
 # Game over, print outcome
-if lettersLeft < 0:
+if letters_left == 0:
     print("YOU WIN!!")
 else:
-    print("YOU LOSE...word was " + targetWord)
+    print("YOU LOSE...word was " + target_word)
