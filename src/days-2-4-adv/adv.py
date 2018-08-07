af8f8f617197b304076102ff0ba9ef66a1a74563
Stephanie Bowles <sbowl001@fiu.edu>
2018-08-07 18:57:20 +0900

    adding self-created rooms

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -4,36 +4,80 @@ from player import Player
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
+    'village':  Room("Village Hidden in the Spring",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'home':    Room("Home Sweet Home", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'attic': Room("Attic Room", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'forest':   Room("Grand Forest", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+   'overlook':   Room("Mountain Overlook", """A steep mountain appears before you. The forest expands in all directions. Perhaps a further look is needed..."""),
+
+   'peak':   Room("Mountain Peak", """The fresh air soothes your senses, the forest expanse looks peaceful. The village looks minute in the distance."""),
+
+   'cave':   Room("Narrow Passage", """The narrow passage bends here from west
+to north. The smell of gold permeates the air."""),
+
+   'underground1':   Room("Underground Passage", """The narrow passage bends here from west
+to north. The smell of gold permeates the air."""),
+
+   'tunnels':   Room("Dark Tunnels", """The narrow passage bends here from west
+to north. The smell of gold permeates the air."""),
+
+   'secret':   Room("Secret Passage", """The passage holds an air of mystery. The walls glimmer with something hidden beneath."""),
+
+   'epic':   Room("Epic Treasure", """The room contains the most special of treasure chests. Open it to reveal your prize."""),
+
+   'underground2':   Room("Damp Underground Passage", """Moist droplets of water fall from the passage's crevices."""),
+
+    'dragon': Room("Dragon Lair", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
+
+   'swamp':   Room("Sinister Swamp", """The narrow passage bends here from west
+to north. The smell of gold permeates the air."""),
+
+   'mangroves':   Room("Mangrove Forest", """The narrow passage bends here from west
+to north. The smell of gold permeates the air."""),
 }
 
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
-
+room['village'].n_to = room['forest']
+room['forest'].s_to = room['village']
+room['village'].s_to = room['home']
+room['home'].n_to = room['village']
+room['home'].u_to = room['attic']
+room['attic'].d_to = room['home']
+room['forest'].w_to = room['overlook']
+room['overlook'].e_to = room['forest']
+room['overlook'].u_to = room['peak']
+room['peak'].d_to = room['overlook']
+room['forest'].e_to = room['mangroves']
+room['mangroves'].w_to = room['forest']
+room['forest'].n_to = room['cave']
+room['cave'].s_to = room['forest']
+room['cave'].d_to = room['underground1']
+room['underground1'].u_to = room['cave']
+room['underground1'].e_to = room['tunnels']
+room['tunnels'].w_to = room['underground1']
+room['tunnels'].e_to = room['dragon']
+room['dragon'].w_to = room['tunnels']
+room['tunnels'].n_to = room['secret']
+room['secret'].s_to = room['tunnels']
+room['tunnels'].s_to = room['underground2']
+room['underground2'].n_to = room['tunnels']
+room['underground2'].u_to = room['swamp']
+room['swamp'].d_to = room['underground2']
+room['swamp'].n_to = room['mangroves']
+room['mangroves'].s_to = room['swamp']
 #
 # Main
 #
